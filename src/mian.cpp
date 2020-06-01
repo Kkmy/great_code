19529eba80a18dc6029a2f617cf254435ab800e9
Kei Hakoda <kankamichan@gmail.com>
2020-06-01 17:23:29 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -1,9 +1,15 @@
-﻿char mojiretsu[1000];
-int mojisuu = 0;
+char mojiretsu[1000];
+int number = 0;
+
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
+
 #ifdef this_is_a_great_codo
-問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
+	問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、15でならFizzBuzzと発言する
 #endif
-int main() { while (mojisuu++ < 1000)sprintf(mojiretsu, "%d", mojisuu), printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); system("PAUSE"); }
+int main() {
+	while(number++ < 1000) sprintf(mojiretsu, "%d", mojisuu),
+	printf("%s, ", number % 3 ? number % 5 ? number % 15 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz");
+	system("PAUSE");
+}
