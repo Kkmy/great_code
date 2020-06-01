c8ffc9db05f13ea2e1a8b5b38d0207c3af322123
Kei Hakoda <kankamichan@gmail.com>
2020-06-01 17:24:24 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -8,6 +8,7 @@ int number = 0;
 #ifdef this_is_a_great_codo
 	問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、15でならFizzBuzzと発言する
 #endif
+
 int main() {
 	while(number++ < 1000) sprintf(mojiretsu, "%d", mojisuu),
 	printf("%s, ", number % 3 ? number % 5 ? number % 15 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz");
